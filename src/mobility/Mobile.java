a11a02b35a9e5326c26ebd61d4d0adc2dae7d620
reutshukrun <reutshukrun449@gmail.com>
2017-04-07 10:51:52 +0300

    change mobile class

--- src/mobility/Mobile.java
+++ src/mobility/Mobile.java
@@ -1,7 +1,7 @@
 package mobility;
 
 import mobility.Point;
-
+import java.lang.Math;;
 public class Mobile implements ILocatable
 {
 	private Point location;
@@ -9,8 +9,6 @@ public class Mobile implements ILocatable
 	
 	public Mobile(Point location)
 	{
-		//לשאול את דולב!
-		//this.location = location;
 		this.location = new Point(location);
 	}
 	public void addTotalDistance(double dist)
@@ -18,19 +16,19 @@ public class Mobile implements ILocatable
 		this.totalDistance+=dist;
 	}
 	
-	/*public double calcDistance(Point newPoint)
+	public double calcDistance(Point newPoint)
 	{
-		
+		return Math.sqrt( Math.pow(newPoint.getX()-this.location.getX(),2)+Math.pow(newPoint.getY()-this.location.getY(),2));
 		
 	}
-	public double move(Point newPoint)
+	/*public double move(Point newPoint)
 	{
 	
-		//Dolev
+		//Dolev-equals in Point is Copy-Constructor
 	if(this.location.equals(newPoint))
 			return 0;
 	return clacDistance(newPoint);
-	}
+	}*/
 	
 	
 	public Point getLocation()
@@ -41,7 +39,11 @@ public class Mobile implements ILocatable
 	
 	public boolean setLocation(Point newLocation)
 	{
-		
+		if(newLocation==null)
+			return false;
+		this.location.setX(newLocation.getX());
+		this.location.setY(newLocation.getY());
+		return true;	
 	}
-*/
+
 }
