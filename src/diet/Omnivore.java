34e3a2ccf7aaabd59bb3a011aae92e694fd10521
reutshukrun <reutshukrun449@gmail.com>
2017-04-16 10:54:53 +0300

    CHANGE OMNIVORE

--- src/diet/Omnivore.java
+++ src/diet/Omnivore.java
@@ -10,7 +10,9 @@ public class Omnivore implements IDiet
 	public boolean canEat(EFoodType food)
 	{
 		if ((food.equals(EFoodType.MEAT))||food.equals(EFoodType.NOTFOOD))
+		{
 			return true;
+		}
 		return false;
 	}
 		
