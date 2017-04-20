1bc11ac126d0284291e192da1e2cc250de6bdb5c
dolev <mrdolbyking@gmail.com>
2017-04-20 08:34:28 +0300

    fixing problem

--- src/mobility/Mobile.java
+++ src/mobility/Mobile.java
@@ -9,21 +9,40 @@ public class Mobile implements ILocatable
 {
 	private Point location;
 	private double totalDistance;
-	
+	/**
+	 * Constructor-Mobile:Intializing the location of the animal according the location received
+	 * @param location
+	 * 		new location to the animal
+	 */
 	public Mobile(Point location)
 	{
 		this.location = new Point(location);
 	}
+	/**
+	 * addTotalDistancde- updating the total distance the animal passed by adding the last distance(dist)to the total distance exist.
+	 * @param dist-
+	 * 		the distance the animal passed between the old Point and the new one.
+	 */
 	public void addTotalDistance(double dist)
 	{
 		this.totalDistance+=dist;
 	}
-	
+	/**
+	 * calcDistance- calculating the distance between the old location of the animal and the new one(received-newPoint)
+	 * @param newPoint
+	 * 		-The new locaion of the animal
+	 * @return the distance which had calculate
+	 */
 	public double calcDistance(Point newPoint)
 	{
 		return Math.sqrt( Math.pow(newPoint.getX()-this.location.getX(),2)+Math.pow(newPoint.getY()-this.location.getY(),2));
 		
 	}
+	/**
+	 * move- If the Point valid- the function move the location of the animal t the new location, and do actions after moving(cange weight,calculate and update distance)
+	 * @param newPoint
+	 * @return
+	 */
 	public double move(Point newPoint)
 	{
 		
@@ -40,17 +59,21 @@ public class Mobile implements ILocatable
 		this.setLocation(newPoint);			
 		this.addTotalDistance(this.calcDistance(newPoint));
 		return this.calcDistance(newPoint);
-	
-	
 	}
 	
-	
+	/**
+	 *@return -The location of the animal
+	 */
 	public Point getLocation()
 	{
 		return this.location;
 	}
 	
-	
+	/**
+	 * If the new location received valid- the function changing the location exist
+	 * @return True- If there was a changing
+	 * 			FAlse- If there wasn't a changing 
+	 */
 	public boolean setLocation(Point newLocation)
 	{
 		if(newLocation==null)
